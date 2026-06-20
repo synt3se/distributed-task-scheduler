4fcbd5f51236b9247378047e65a700100027eed1
ataskaev <ataskaev@yandex.ru>
2026-06-20 19:05:26 +0700

    init project with gradle

---
+++ src/main/java/com/github/synt3se/distributed_task_scheduler/DistributedTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.github.synt3se.distributed_task_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/synt3se/distributed_task_scheduler/DistributedTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.synt3se.distributed_task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
